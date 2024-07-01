47589b7a6d71a83f9ebd92139d5760460cfe9663
w2599 <114452622+w2599@users.noreply.github.com>
2024-07-01 20:47:02 +0800

    Block updates

--- Application/Dopamine/UI/DOUIManager.m
+++ Application/Dopamine/UI/DOUIManager.m
@@ -37,6 +37,7 @@
 
 - (BOOL)isUpdateAvailable
 {
+    return NO;
     NSString *latestVersion = [self getLatestReleaseTag];
     NSString *currentVersion = [self getLaunchedReleaseTag];
     return [latestVersion numericalVersionRepresentation] > [currentVersion numericalVersionRepresentation];
@@ -88,6 +89,7 @@
 
 - (BOOL)environmentUpdateAvailable
 {
+    return NO;
     if (![[DOEnvironmentManager sharedManager] jailbrokenVersion])
         return NO;
 
