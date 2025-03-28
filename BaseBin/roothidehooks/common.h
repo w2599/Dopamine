06a732c1cdd0e086c5c7b2230dd3525d959d6278
w2599 <114452622+w2599@users.noreply.github.com>
2025-03-28 20:13:03 +0800

    hide more

--- BaseBin/roothidehooks/common.h
+++ BaseBin/roothidehooks/common.h
@@ -15,4 +15,9 @@ bool isJailbreakPath(const char* path);
     @"com.opa334.Dopamine", \
     @"com.opa334.Dopamine.roothide", \
     @"com.opa334.Dopamine-roothide", \
+    @"com.zqbb.Dopamine", \
+    @"com.zqbb.Dopamine.roothide", \
+    @"com.zqbb.Dopamine-roothide", \
+    @"wiki.qaq.TrollFools", \
+    @"com.netskao.dumpdecrypter", \
 ]
