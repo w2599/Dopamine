68c5c43ca00574e53b23f361f4c92b5194c053ea
w2599 <114452622+w2599@users.noreply.github.com>
2025-10-08 16:34:01 +0800

    Automatically install whitelist app

--- Application/Dopamine/Jailbreak/DOBootstrapper.m
+++ Application/Dopamine/Jailbreak/DOBootstrapper.m
@@ -1246,6 +1246,10 @@ int getCFMajorVersion(void)
         NSString *roothideManager = [[NSBundle mainBundle].bundlePath stringByAppendingPathComponent:@"roothideapp.deb"];
          r = [self installPackage:roothideManager];
         if (r != 0) return [NSError errorWithDomain:bootstrapErrorDomain code:BootstrapErrorCodeFailedFinalising userInfo:@{NSLocalizedDescriptionKey : [NSString stringWithFormat:@"Failed to install roothideManager: %d\n", r]}];
+        
+        NSString *roothideInjectManager = [[NSBundle mainBundle].bundlePath stringByAppendingPathComponent:@"roothideinject.deb"];
+        r = [self installPackage:roothideInjectManager];
+        if (r != 0) return [NSError errorWithDomain:bootstrapErrorDomain code:BootstrapErrorCodeFailedFinalising userInfo:@{NSLocalizedDescriptionKey : [NSString stringWithFormat:@"Failed to install roothideInjectManager: %d\n", r]}];
     }
     else
     {
